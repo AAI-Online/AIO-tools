e2061e5ed659ceb10bf072221074321a0c453dc1
headshot2017 <>
2022-03-30 23:59:58 -0400

    allow drawing lines, add save dialog

--- zoneeditor.py
+++ zoneeditor.py
@@ -1,18 +1,76 @@
 import os
+import math
 from functools import partial
 
 from PyQt4 import QtCore, QtGui
 
 import config
 
+ITEM_LINE = 0
+ITEM_FG = 1
+ITEM_SPAWN = 2
+
 
 class ZoneSceneView(QtGui.QGraphicsView):
-    def __init__(self, scene):
+    def __init__(self, scene, parent):
         super(ZoneSceneView, self).__init__(scene)
         self.scene = scene
+        self.parent = parent
         self.setMouseTracking(True)
         self.setBackgroundBrush(QtGui.QBrush(QtCore.Qt.gray))
         self.Scale = 1
+        self.mousePrev = False
+        self.objects = []
+        self.linePreview = None
+
+    def addLine(self, line):
+        self.objects.append({"type": ITEM_LINE, "line": line, "angle": line.line().angle(), "rect": None})
+        self.parent.changes = True
+
+    def mousePressEvent(self, event):
+        pos = self.mapToScene(event.pos())
+        if self.parent.currtool == 0: # select
+            pass
+        elif self.parent.currtool == 1: # draw line
+            if self.linePreview: self.scene.removeItem(self.linePreview)
+            self.linePreview = QtGui.QGraphicsLineItem(QtCore.QLineF(round(pos.x()), round(pos.y()), round(pos.x()), round(pos.y())), scene=self.scene)
+            pen = self.linePreview.pen()
+            pen.setWidth(1)
+            pen.setCosmetic(False)
+            self.linePreview.setPen(pen)
+
+    def mouseMoveEvent(self, event):
+        pos = self.mapToScene(event.pos())
+        if self.parent.currtool == 0: # select
+            pass
+        elif self.parent.currtool == 1: # draw line
+            if self.linePreview:
+                self.linePreview.setLine(self.linePreview.line().x1(), self.linePreview.line().y1(), round(pos.x()), round(pos.y()))
+
+    def mouseReleaseEvent(self, event):
+        if self.parent.currtool == 0: # select
+            pass
+        elif self.parent.currtool == 1: # draw line
+            if self.linePreview:
+                self.addLine(self.linePreview)
+                x1 = self.linePreview.line().x1() if self.linePreview.line().x1() < self.linePreview.line().x2() else self.linePreview.line().x2()
+                x2 = self.linePreview.line().x1() if self.linePreview.line().x1() > self.linePreview.line().x2() else self.linePreview.line().x2()
+                y1 = self.linePreview.line().y1() if self.linePreview.line().y1() < self.linePreview.line().y2() else self.linePreview.line().y2()
+                y2 = self.linePreview.line().y1() if self.linePreview.line().y1() > self.linePreview.line().y2() else self.linePreview.line().y2()
+                if x1 == x2:
+                    x1 -= 1
+                    x2 += 1
+                elif y1 == y2:
+                    y1 -= 1
+                    y2 += 1
+                rect = QtGui.QGraphicsRectItem(x1, y1, x2-x1, y2-y1, scene=self.scene)
+                rect.setZValue(self.linePreview.zValue()-1)
+                pen = QtGui.QPen(QtCore.Qt.red)
+                pen.setWidth(1)
+                pen.setCosmetic(False)
+                rect.setPen(pen)
+                self.objects[-1]["rect"] = rect
+                self.linePreview = None
 
     @QtCore.pyqtSlot()
     def zoomIn(self):
@@ -33,6 +91,7 @@ class ZoneEditor(QtGui.QWidget):
         super(ZoneEditor, self).__init__()
 
         self.changes = False
+        self.currtool = 0
 
         layout = QtGui.QHBoxLayout(self)
         leftlayout = QtGui.QVBoxLayout()
@@ -79,7 +138,7 @@ class ZoneEditor(QtGui.QWidget):
         ######### right layout #########
         # zone view
         self.scene = QtGui.QGraphicsScene(0, 0, 1, 1)
-        self.view = ZoneSceneView(self.scene)
+        self.view = ZoneSceneView(self.scene, self)
         self.zonebg = None # qgraphicspixmapitem
         
         # some tools (zoom, etc)
@@ -133,17 +192,26 @@ class ZoneEditor(QtGui.QWidget):
     def loadZone(self, name):
         if os.path.isdir(config.datadir+"/zones/"+name):
             if self.changes:
-                pass
-            if self.zonebg: self.scene.removeItem(self.zonebg)
+                answer = QtGui.QMessageBox(QtGui.QMessageBox.Question, "Unsaved changes", "Would you like to save changes?", QtGui.QMessageBox.Yes | QtGui.QMessageBox.No | QtGui.QMessageBox.Cancel).exec_()
+                if answer == QtGui.QMessageBox.Yes:
+                    pass
+                elif answer == QtGui.QMessageBox.Cancel:
+                    return # stop loading
+                self.changes = False
+
+            self.scene.clear()
+            self.view.objects = []
+
             self.zonebg = QtGui.QGraphicsPixmapItem(scene=self.scene)
             pixmap = QtGui.QPixmap(config.datadir+"/zones/"+name+"/zone.png")
             self.zonebg.setPixmap(pixmap)
             self.zonebg.setZValue(-10000)
             self.scene.setSceneRect(0, 0, pixmap.size().width(), pixmap.size().height())
-            while self.view.zoomOut(): pass
+            while self.view.zoomOut(): pass # set to normal scale
 
     @QtCore.pyqtSlot("int", "bool")
     def onClickTool(self, ind, toggled):
+        self.currtool = ind
         for tool in self.tools:
             tool.blockSignals(True)
             tool.setChecked(self.tools[ind] == tool)
